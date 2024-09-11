11a97d01f81f28aff5d9d2b969a87e449cbb6065
DellalMohamed <72273147+DellalMohamed@users.noreply.github.com>
2024-09-11 22:37:31 +0100

    delete fontawsome code

--- src/main.js
+++ src/main.js
@@ -2,8 +2,5 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/css/main.css";
-//import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-library.add(fas);
+
 createApp(App).use(router).mount("#app");
